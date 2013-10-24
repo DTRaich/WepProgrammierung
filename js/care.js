1862f3c9c753e83626a234a4ea33e23c1e972b1a
SinaS93 <sina.steck@googlemail.com>
2013-10-24 16:15:03 +0200

    Pflege-Daten Selektion
    Daten selektiert und vorerst in Popup anziegen

--- js/care.js
+++ js/care.js
@@ -5,8 +5,8 @@ var isSeen = false;
 
 // ---------------------------------------standart care Form----------------------
 var careFormTemplate = _.template('<br> <h1> Neuen Film hinzufügen </h1> <br><br> '+ 
-									'Filmtitel:<input type="text" placeholder="Filmtitel" size="50" id="movietitle"/> <br> <br> ' +
-									'Erscheinungsjahr:<input type="text" placeholder="Jahr" size="4" id="year" maxlength="4" /> <br> <br> '+
+									'Filmtitel:<input type="text" placeholder="Filmtitel" size="50" id="movietitle" class="care_textbox" /> <br> <br> ' +
+									'Erscheinungsjahr:<input type="number" placeholder="Jahr" size="4" id="year" maxlength="4" /> <br> <br> '+
 									'Genre: <select id="genre_select">'+
 												'<option value="null">- Genre -</option>' +
 												'<option value="Action">Action</option>' +
@@ -23,15 +23,12 @@ var sendButtonTemplate = _.template('<input type="button" name="addFilm" id="add
 
 //------------------------------------- rate and Commen Template which is displayed between them----------									
 var rateAndCommentTemplate = _.template(' Bewertung : <select id="rating">'+
-
 										'  <option  value ="1"> &#9733</option> '+
 										'  <option  value ="2"> &#9733 &#9733</option> '+
 										'  <option  value ="3"> &#9733 &#9733 &#9733</option> '+
 										'  <option  value ="4"> &#9733 &#9733 &#9733 &#9733</option> '+
 										'  <option  value ="5"> &#9733 &#9733 &#9733 &#9733 &#9733</option> '+	  
 										' </select> <br><br>');
-
-
 										
 //------------------------------------ PUBLIC METHODS TO CALL--------------------------
 
@@ -71,11 +68,24 @@ $(document).ready(function(){
 	//addMovie Method
 	$(document).on('click', '#addMovie', function(event) {
 		
-			var titel=$.trim($('#ID').val());		
+			var title_input = $.trim($('#movietitle').val());
+			var year_input = "1999";
+			var rating_input;
+			var genreEl = document.getElementById("genre_select");
+			var genre_input = genreEl.options[genreEl.selectedIndex].value;
 			
-		
+			if(isSeen===true){
+			var ratingEl = document.getElementById("rating");
+			rating_input = ratingEl.options[ratingEl.selectedIndex].value;
+			}
+			else{
+			rating_input = "0";
+			}
+			
+			alert("Titel: " + title_input + "\n" +  "Genre: " + genre_input + "\n" + "Rating: " + rating_input );
+			
 			event.preventDefault();
 			event.stopImmediatePropagation();
-			});	
+			});
 
 })
