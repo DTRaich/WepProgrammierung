074d6dd534b3f3e92f66915c7339c063d89adb81
SinaS93 <sina.steck@googlemail.com>
2013-10-25 09:57:58 +0200

    Pflege3
    Textfelder eingerückt

--- js/care.js
+++ js/care.js
@@ -1,13 +1,14 @@
 //CareScript 
 //----------------------------------------GLOBAL VARIABLES-------------------------
 var isSeen = false;
+// &nbsp leerzeichen
 //-------------------------------------------TEMPLATES-------------------------------
 
 // ---------------------------------------standart care Form----------------------
 var careFormTemplate = _.template('<br> <h1> Neuen Film hinzufügen </h1> <br><br> '+ 
-									'Filmtitel:<input type="text" placeholder="Filmtitel" size="50" id="movietitle" class="care_textbox" /> <br> <br> ' +
-									'Erscheinungsjahr:<input type="number" placeholder="Jahr" size="4" id="year" maxlength="4" /> <br> <br> '+
-									'Genre: <select id="genre_select">'+
+									'Filmtitel: &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <input type="text" placeholder="Filmtitel" size="50" id="movietitle" class="care_textbox"/> <br> <br> ' +
+									'Erscheinungsjahr: &nbsp&nbsp&nbsp <input type="number" placeholder="Jahr" size="4" id="year" maxlength="4" class="care_textbox" /> <br> <br> '+
+									'Genre: &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <select id="genre_select" class="care_textbox">'+
 												'<option value="null">- Genre -</option>' +
 												'<option value="Action">Action</option>' +
 												'<option value="Comedy">Comedy</option>' +
@@ -15,14 +16,14 @@ var careFormTemplate = _.template('<br> <h1> Neuen Film hinzufügen </h1> <br><br
 												'<option value="Sci-Fi">Sci-Fi</option>' +
 												'<option value="Thriller">Thriller</option>' +
 											'</select> <br><br>' +
-									'Gesehen: <input id="ratingChb" name"chbRating" type="checkbox" name="Gesehen:" value="seen" / ><br><br>');
+									'Gesehen: &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <input id="ratingChb" name"chbRating" type="checkbox" name="Gesehen:" value="seen" class="care_textbox"/ ><br><br>');
 									
 //-----------------------------------------button to sen/add the movie ---------------------									
 var sendButtonTemplate = _.template('<input type="button" name="addFilm" id="addMovie" value="Film hinzufügen" / >');
 
 
 //------------------------------------- rate and Commen Template which is displayed between them----------									
-var rateAndCommentTemplate = _.template(' Bewertung : <select id="rating">'+
+var rateAndCommentTemplate = _.template(' Bewertung : &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <select id="rating" class="care_textbox">'+
 										'  <option  value ="1"> &#9733</option> '+
 										'  <option  value ="2"> &#9733 &#9733</option> '+
 										'  <option  value ="3"> &#9733 &#9733 &#9733</option> '+
@@ -71,21 +72,31 @@ $(document).ready(function(){
 			var title_input = $.trim($('#movietitle').val());
 			var year_input = "1999";
 			var rating_input;
+			var isSeen_input;
 			var genreEl = document.getElementById("genre_select");
 			var genre_input = genreEl.options[genreEl.selectedIndex].value;
 			
 			if(isSeen===true){
 			var ratingEl = document.getElementById("rating");
 			rating_input = ratingEl.options[ratingEl.selectedIndex].value;
+			isSeen_input = true;
 			}
 			else{
 			rating_input = "0";
+			isSeen_input = false;
 			}
 			
-			alert("Titel: " + title_input + "\n" +  "Genre: " + genre_input + "\n" + "Rating: " + rating_input );
+			alert("Titel: " + title_input + "\n" +  "Genre: " + genre_input + "\n" + "Rating: " + rating_input + "\n" + "Gesehen: " + isSeen_input);
+			
+			if( genre_input == "null" ){
+			alert('Bitte ein Genre auswählen');
+			}
+			
 			
 			event.preventDefault();
 			event.stopImmediatePropagation();
 			});
+			
+			
 
 })
